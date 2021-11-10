c93552cbef797deb9932cb78cca1d4fc5cb14adb
wanyixuan <wanyixuan@didichuxing.com>
2021-11-10 11:51:30 +0800

    feat: build express server & init axios demo

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,18 @@
-export type MethodType = 'get' | 'Get'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
   url: string
